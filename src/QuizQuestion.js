2a3a80cc5720c0a68451f4b9f667762999831010
Abigayle Franklin <Abifranklin@gmail.com>
2019-12-08 21:10:26 -0600

    Module 6 complete

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,15 +5,17 @@ class QuizQuestion extends Component {
     constructor(props){
         super(props);
         this.state = {
-            quiz_position: 1
+            incorrectAnswer: false
         }
     }
 
     handleClick(buttonText) {
-        return (
-            buttonText === this.props.quiz_question.answer ?
-            this.props.showNextQuestionHandler() : null
-        )
+        if (buttonText === this.props.quiz_question.answer) {
+        this.props.showNextQuestionHandler()
+        this.setState({ incorrectAnswer: false })
+        } else {
+        this.setState({ incorrectAnswer: true })
+        }
     }
 
     render(){
@@ -34,6 +36,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right"</p> : null }
             </main>
         )
     }
